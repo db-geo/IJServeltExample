c8ac4067316d8e78126cb99c98cb1f0b886b2d4d
admin <dominique.bertin@geonosis.fr>
2020-03-13 12:48:01 +0100

    MySQL Timezone

--- src/main/java/fr/imt/cepi/servlet/listeners/AppContextListener.java
+++ src/main/java/fr/imt/cepi/servlet/listeners/AppContextListener.java
@@ -17,55 +17,54 @@ import fr.imt.cepi.util.DBConnectionManager;
 @WebListener
 public class AppContextListener implements ServletContextListener {
 
-	@Override
-	public void contextInitialized(ServletContextEvent servletContextEvent) {
-		ServletContext ctx = servletContextEvent.getServletContext();
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext ctx = servletContextEvent.getServletContext();
 
-		// initialize DB Connection
-		String dbURL = ctx.getInitParameter("dbURL");
-		String user = ctx.getInitParameter("dbUser");
-		String pwd = ctx.getInitParameter("dbPassword");
+        // initialize DB Connection
+        String dbURL = ctx.getInitParameter("dbURL");
+        String user = ctx.getInitParameter("dbUser");
+        String pwd = ctx.getInitParameter("dbPassword");
 
-		try {
-			DBConnectionManager connectionManager = new DBConnectionManager(dbURL, user, pwd);
-			ctx.setAttribute("DBConnection", connectionManager.getConnection());
-			System.out.println("Connection à la base de données effectuée avec succès");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+        try {
+            DBConnectionManager connectionManager = new DBConnectionManager(dbURL +
+                    "?useLegacyDatetimeCode=false&serverTimezone=Europ/Paris", user, pwd);
+            ctx.setAttribute("DBConnection", connectionManager.getConnection());
+            System.out.println("Connection à la base de données effectuée avec succès");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		// initialize log4j
-		String log4jConfig = ctx.getInitParameter("log4j-config");
-		if (log4jConfig == null) {
-			System.err.println(
-					"Pas de paramètre log4j-config trouvé dans le fichier web.xml : initialisation de log4j avec la configuration de base");
-			BasicConfigurator.configure();
-		} else {
-			String webAppPath = ctx.getRealPath("/");
-			String log4jProp = webAppPath + log4jConfig;
-			File log4jConfigFile = new File(log4jProp);
-			if (log4jConfigFile.exists()) {
-				System.out.println("Initialisation de log4j avec le fichier  " + log4jProp);
-				DOMConfigurator.configure(log4jProp);
-			} else {
-				System.err.println(
-						log4jProp + " fichier non trouvé : initialisation de log4j avec la configuration de base");
-				BasicConfigurator.configure();
-			}
-		}
-		System.out.println("log4j configuré correctement");
-	}
+        // initialize log4j
+        String log4jConfig = ctx.getInitParameter("log4j-config");
+        if (log4jConfig == null) {
+            System.err.println(
+                    "Pas de paramètre log4j-config trouvé dans le fichier web.xml : initialisation de log4j avec la configuration de base");
+            BasicConfigurator.configure();
+        } else {
+            String webAppPath = ctx.getRealPath("/");
+            String log4jProp = webAppPath + log4jConfig;
+            File log4jConfigFile = new File(log4jProp);
+            if (log4jConfigFile.exists()) {
+                System.out.println("Initialisation de log4j avec le fichier  " + log4jProp);
+                DOMConfigurator.configure(log4jProp);
+            } else {
+                System.err.println(
+                        log4jProp + " fichier non trouvé : initialisation de log4j avec la configuration de base");
+                BasicConfigurator.configure();
+            }
+        }
+        System.out.println("log4j configuré correctement");
+    }
 
-	@Override
-	public void contextDestroyed(ServletContextEvent servletContextEvent) {
-		Connection con = (Connection) servletContextEvent.getServletContext().getAttribute("DBConnection");
-		try {
-			con.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        Connection con = (Connection) servletContextEvent.getServletContext().getAttribute("DBConnection");
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
